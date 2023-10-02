3c6537dac608a7246d11bb9f2f910267811566b8
Karthik S <karthik@Karthiks-MacBook-Air.local>
2023-10-02 17:51:46 -0400

    Adding initial version of MinMaxCalculation

---
+++ MinMaxCalculation.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class MinMaxCalculation {
+    ArrayList< Float > numList;
+
+    public MinMaxCalculation( ) {
+      this.numList = new ArrayList< Float >();
+    }
+  
+    public void returnInputValidation( ArrayList< Float > numberList ) {
+        float maximumVal = Collections.max( numberList );
+        System.out.println("Maximum is : " + maximumVal );
+    }
+}
